8e6c16f70f714d4f4fdac901f5c0f2c18c2c1bf7
崎璇 王 <mightyactionx@163.com>
2020-02-24 18:18:09 +0800

    新建项目

---
+++ src/main/java/com/whu/bookstore/Common/Result.java
@@ -0,0 +1,10 @@
+package com.whu.bookstore.Common;
+
+/**
+ * @author ：qx.w
+ * @description：返回值对象
+ * @modified By：
+ * @since ：Created in 2020/2/24 17:54
+ */
+public class Result {
+}
---
+++ src/main/java/com/whu/bookstore/Config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.whu.bookstore.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author ：qx.w
+ * @description：
+ * @modified By：
+ * @since ：Created in 2020/2/24 17:55
+ */
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); //允许任何域名
+        corsConfiguration.addAllowedHeader("*"); //允许任何头
+        corsConfiguration.addAllowedMethod("*"); //允许任何方法
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); //注册
+        return new CorsFilter(source);
+    }
+}
